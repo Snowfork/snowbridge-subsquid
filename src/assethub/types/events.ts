b44fdbf0b7e6ddaca73ed77136d65482371eebf4
ron <yrong1997@gmail.com>
2024-06-18 17:14:40 +0800

    generate types for assethub

---
+++ src/assethub/types/events.ts
@@ -0,0 +1,2 @@
+export * as foreignAssets from './foreign-assets/events'
+export * as messageQueue from './message-queue/events'
---
+++ src/assethub/types/index.ts
@@ -0,0 +1,3 @@
+export * as v9430 from './v9430'
+export * as v1002000 from './v1002000'
+export * as events from './events'
