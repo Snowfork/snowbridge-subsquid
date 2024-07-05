05d1fb9a5206d24d50e928fdc2a6200e6974a798
ron <yrong1997@gmail.com>
2024-07-05 00:43:36 +0000

    Make channel_id as arg

--- src/server-extension/resolvers/index.ts
+++ src/server-extension/resolvers/index.ts
@@ -1,6 +1,9 @@
 import "reflect-metadata";
-import { Field, ObjectType, Query, Resolver } from "type-graphql";
+import { Arg, Field, ObjectType, Query, Resolver } from "type-graphql";
 import type { EntityManager } from "typeorm";
+
+const AssetHubChannelId =
+  "0xc173fac324158e77fb5840738a1a541f633cbec8884c6a601c567d2b376a0539";
 
 @ObjectType()
 export class ElapseResult {
@@ -13,14 +16,21 @@ export class TransferElapseResolver {
   constructor(private tx: () => Promise<EntityManager>) {}
 
   @Query(() => ElapseResult)
-  async toPolkadotElapse(): Promise<ElapseResult> {
+  async toPolkadotElapse(
+    @Arg("channelId", {
+      nullable: true,
+      defaultValue: AssetHubChannelId,
+    })
+    channelId: string
+  ): Promise<ElapseResult> {
     const manager = await this.tx();
 
     const query = `with to_polkadot_elapse as
     (
         select transfer_status_to_polkadot.timestamp as ts1, message_processed_on_polkadot.timestamp as ts2 
         from transfer_status_to_polkadot join message_processed_on_polkadot 
-        on transfer_status_to_polkadot.message_id = message_processed_on_polkadot.message_id 
+        on transfer_status_to_polkadot.message_id = message_processed_on_polkadot.message_id
+        where transfer_status_to_polkadot.channel_id = '${channelId}'
     )
     SELECT EXTRACT(EPOCH FROM (select avg(ts2 - ts1) from to_polkadot_elapse)) as elapse
     `;
@@ -30,14 +40,21 @@ export class TransferElapseResolver {
   }
 
   @Query(() => ElapseResult)
-  async toEthereumElapse(): Promise<ElapseResult> {
+  async toEthereumElapse(
+    @Arg("channelId", {
+      nullable: true,
+      defaultValue: AssetHubChannelId,
+    })
+    channelId: string
+  ): Promise<ElapseResult> {
     const manager = await this.tx();
 
     const query = `with to_ethereum_elapse as
     (
         select transfer_status_to_ethereum.timestamp as ts1, inbound_message_dispatched_on_ethereum.timestamp as ts2 
         from transfer_status_to_ethereum join inbound_message_dispatched_on_ethereum 
-        on transfer_status_to_ethereum.message_id = inbound_message_dispatched_on_ethereum.message_id 
+        on transfer_status_to_ethereum.message_id = inbound_message_dispatched_on_ethereum.message_id
+        where transfer_status_to_ethereum.channel_id = '${channelId}' 
     )
     SELECT EXTRACT(EPOCH FROM (select avg(ts2 - ts1) from to_ethereum_elapse)) as elapse
     `;
